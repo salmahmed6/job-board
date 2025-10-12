27143b924b25114259df3977c3c13e04094cc894
salmahmed6 <sa1963645@gmail.com>
2025-10-12 16:21:37 +0300

    Refactor routes to use IndexController for homepage, about, and contact views

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/about', [IndexController::class, 'about']);
+Route::get('/contact', [IndexController::class, 'contact']);
 
 Route::get('/job', [JobController::class, 'index']);
