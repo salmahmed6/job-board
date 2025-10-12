456fa42f061a9157155082e86e58e7a7f39ba1bb
salmahmed6 <sa1963645@gmail.com>
2025-10-12 16:21:28 +0300

    Add About and Contact pages, update Job Board layout, and remove Welcome page

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,13 +1,3 @@
-<div>
-
-    <h1>Job Board</h1>
-
-    @foreach ($jobs as $job)
-        <div>
-            <h2>{{ $job['title'] }}</h2>
-            <p>Company: {{ $job['company'] }}</p>
-            <p>Location: {{ $job['location'] }}</p>
-        </div>
-    @endforeach
-
-</div>
+<x-layout>
+    <h2>Home Page</h2>
+</x-layout>
