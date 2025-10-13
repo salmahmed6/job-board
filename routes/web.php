852b5f2f6fd53539f3d47efa20bf69e8e8583b9f
salmahmed6 <sa1963645@gmail.com>
2025-10-13 10:27:46 +0300

    Add routes for PostController to manage posts

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/about', [IndexController::class, 'about']);
 Route::get('/contact', [IndexController::class, 'contact']);
-
 Route::get('/job', [JobController::class, 'index']);
+
+Route::get('/post', [PostController::class,'index']);
+Route::get('/post/create', [PostController::class,'create']);
+Route::get('/post/{id}', [PostController::class,'show']);
