faeba9994aa73a2cd127d05ec05eb83b643613d4
salmahmed6 <sa1963645@gmail.com>
2025-10-13 13:52:52 +0300

    Modify post table to use UUIDs as primary keys

---
+++ database/migrations/2025_10_13_104841_modify_post_table_to_use_uuid.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('post', function (Blueprint $table) {
+            $table->dropColumn('id'); // Drop the existing integer ID column
+
+            $table->uuid('id')->primary();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('post', function (Blueprint $table) {
+            $table->dropColumn('id');
+
+            $table->id();
+        });
+    }
+};
